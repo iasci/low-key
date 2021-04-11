83213e0f9bd5750eb15c0fbc5ad9e2c0d125e9ef
Michael S <seth.h.dev@gmail.com>
2021-04-11 23:47:59 +0800

    Use functions instead of a class

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,10 +1,28 @@
-import { Card } from "./index"
+import { assert } from "console"
+import { createCard, processCard } from "./index"
 
 describe("review testing", () => {
-  const sampleCard = new Card()
-  const responses: boolean[] = Array(10).fill(true)
-  responses.forEach((response) => {
-    const time = sampleCard.answer(response)
-    console.log(time)
+  const config = {
+    NEW_STEPS_MINUTES: [1, 10],
+    GRADUATING_INTERVAL_DAYS: 1,
+    INTERVAL_MODIFIER_PERCENT: 192,
+    MAXIMUM_INTERVAL_DAYS: 36500,
+    LAPSES_STEPS_MINUTES: [10],
+    NEW_INTERVAL_PERCENT: 50,
+    MINIMUM_INTERVAL_DAYS: 1,
+  }
+
+  it("returns the correct intervals", () => {
+    let card = createCard()
+    const answers: boolean[] = Array(10).fill(true)
+    const intervals = ["0.007", 1, 3, 7, 16, 39, 97, 242, 604, 1507]
+    answers.forEach((answer, index) => {
+      card = processCard(card, answer, config)
+      if (index === 0) {
+        assert(card.nextInterval.toFixed(3) === intervals[0])
+      } else {
+        assert(Math.ceil(card.nextInterval) === intervals[index])
+      }
+    })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,114 @@
 import { defaults } from "./defaults"
 import { ICardConfig } from "./types"
 
-export class Card {
+export interface ICard {
   status: "learned" | "learning" | "relearning"
   stepsIndex: number
   interval: number | null
-  config: ICardConfig
+  nextInterval: number
+}
 
-  /**
-   *
-   * @param config Anki configuration for this card
-   */
-  constructor(config?: Partial<ICardConfig>) {
-    this.status = "learning"
-    this.stepsIndex = 0
-    this.interval = null
+export function createCard(): ICard {
+  return {
+    status: "learning",
+    stepsIndex: 0,
+    interval: null,
+    nextInterval: null,
+  }
+}
 
-    this.config = {
-      ...defaults,
-      ...config,
+export function processCard(
+  card: ICard,
+  correct: boolean,
+  configArg?: Partial<ICardConfig>
+): ICard {
+  const config = {
+    ...defaults,
+    ...configArg,
+  }
+
+  if (card.status === "learned") {
+    if (correct) {
+      const interval =
+        (((card.interval * 130) / 100) * config.INTERVAL_MODIFIER_PERCENT) / 100
+
+      return {
+        ...card,
+        interval,
+        nextInterval: Math.min(config.MAXIMUM_INTERVAL_DAYS, interval),
+      }
+    } else {
+      return {
+        ...card,
+        status: "relearning",
+        stepsIndex: 0,
+        nextInterval: minutesToDays(config.LAPSES_STEPS_MINUTES[0]),
+      }
     }
   }
 
-  /**
-   * Updates the card's new interval and returns it.
-   *
-   * @param correct If the user answered correctly or not
-   * @returns The new interval
-   */
-  answer(correct: boolean): number {
-    if (this.status === "learned") {
-      if (!correct) {
-        this.status = "relearning"
-        this.stepsIndex = 0
-
-        return this.minutesToDays(this.config.LAPSES_STEPS_MINUTES[0])
+  if (card.status === "learning") {
+    if (correct) {
+      const stepsIndex = card.stepsIndex + 1
+      if (stepsIndex < config.NEW_STEPS_MINUTES.length) {
+        return {
+          ...card,
+          stepsIndex,
+          nextInterval: minutesToDays(config.NEW_STEPS_MINUTES[stepsIndex]),
+        }
       } else {
-        this.interval =
-          (((this.interval * 130) / 100) *
-            this.config.INTERVAL_MODIFIER_PERCENT) /
-          100
-
-        return Math.min(this.config.MAXIMUM_INTERVAL_DAYS, this.interval)
+        const interval = config.GRADUATING_INTERVAL_DAYS
+        return {
+          ...card,
+          status: "learned",
+          interval,
+          nextInterval: interval,
+        }
+      }
+    } else {
+      const stepsIndex = 0
+      return {
+        ...card,
+        stepsIndex,
+        nextInterval: minutesToDays(config.NEW_STEPS_MINUTES[stepsIndex]),
       }
     }
+  }
 
-    if (this.status === "learning") {
-      if (!correct) {
-        this.stepsIndex = 0
-
-        return this.minutesToDays(
-          this.config.NEW_STEPS_MINUTES[this.stepsIndex]
+  if (card.status === "relearning") {
+    if (correct) {
+      const stepsIndex = card.stepsIndex + 1
+      if (stepsIndex < config.LAPSES_STEPS_MINUTES.length) {
+        return {
+          ...card,
+          stepsIndex,
+          nextInterval: minutesToDays(config.LAPSES_STEPS_MINUTES[stepsIndex]),
+        }
+      } else {
+        const interval = Math.max(
+          config.MINIMUM_INTERVAL_DAYS,
+          (card.interval * config.NEW_INTERVAL_PERCENT) / 100
         )
-      } else {
-        this.stepsIndex += 1
-
-        if (this.stepsIndex < this.config.NEW_STEPS_MINUTES.length) {
-          return this.minutesToDays(
-            this.config.NEW_STEPS_MINUTES[this.stepsIndex]
-          )
-        } else {
-          this.status = "learned"
-          this.interval = this.config.GRADUATING_INTERVAL_DAYS
-
-          return this.interval
+        return {
+          ...card,
+          status: "learned",
+          stepsIndex: card.stepsIndex + 1,
+          interval,
+          nextInterval: interval,
         }
       }
-    }
-
-    if (this.status === "relearning") {
-      if (!correct) {
-        this.stepsIndex = 0
-
-        return this.minutesToDays(this.config.LAPSES_STEPS_MINUTES[0])
-      } else {
-        this.stepsIndex += 1
-
-        if (this.stepsIndex < this.config.LAPSES_STEPS_MINUTES.length) {
-          return this.minutesToDays(
-            this.config.LAPSES_STEPS_MINUTES[this.stepsIndex]
-          )
-        } else {
-          this.status = "learned"
-          this.interval = Math.max(
-            this.config.MINIMUM_INTERVAL_DAYS,
-            (this.interval * this.config.NEW_INTERVAL_PERCENT) / 100
-          )
-
-          return this.interval
-        }
+    } else {
+      return {
+        ...card,
+        stepsIndex: 0,
+        nextInterval: minutesToDays(config.LAPSES_STEPS_MINUTES[0]),
       }
     }
-
-    throw new Error(`Unknown card status ${this.status}`)
   }
 
-  minutesToDays(minutes: number): number {
-    return minutes / (60 * 24)
-  }
+  throw new Error(`Unknown card status ${card.status}`)
+}
+
+function minutesToDays(minutes: number): number {
+  return minutes / (60 * 24)
 }
